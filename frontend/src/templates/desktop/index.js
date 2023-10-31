848b25c9ce29e05deb32748df1b11cf8913069c5
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-10-31 19:53:10 +0300

    Update fonts

--- frontend/src/templates/desktop/index.js
+++ frontend/src/templates/desktop/index.js
@@ -1,3 +1,5 @@
 import './../../common/common.scss'
+import './../../components/button/button'
 import './../../components/header/header'
+import './../../components/welcome/welcome'
 import './../../components/footer/footer'

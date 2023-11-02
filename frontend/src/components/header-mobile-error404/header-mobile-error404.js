764ee81a04b9d1494664d3c0d0e191812f5db182
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-11-02 19:34:12 +0300

    Added mobile footer header and page 404

---
+++ frontend/src/components/header-mobile-error404/header-mobile-error404.js
@@ -0,0 +1,13 @@
+import './header-mobile-error404.scss'
+
+document.querySelector('.header-mobile__menu').onclick = () => {
+    document
+        .querySelector('.header-mobile__navigation')
+        .classList.add('header-mobile__navigation_active')
+}
+
+document.querySelector('.header-mobile__close').onclick = () => {
+    document
+        .querySelector('.header-mobile__navigation')
+        .classList.remove('header-mobile__navigation_active')
+}
--- frontend/src/templates/mobile/p404.js
+++ frontend/src/templates/mobile/p404.js
@@ -1,3 +1,4 @@
-import './../../common/common.scss';
-import './../../components/header/header';
-import './../../components/footer/footer';
+import './../../common/common.scss'
+import './../../components/header-mobile-error404/header-mobile-error404'
+import './../../components/page404-mobile/page404-mobile'
+import './../../components/footer-mobile/footer-mobile'

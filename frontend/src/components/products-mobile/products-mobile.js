2d48e7bab6badcd6a89b4e76ef81433eb4d37a95
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-11-03 18:47:53 +0300

    Update mobile view

--- frontend/src/components/products-mobile/products-mobile.js
+++ frontend/src/components/products-mobile/products-mobile.js
@@ -1,1 +1,8 @@
 import './products-mobile.scss'
+
+document.querySelectorAll('.product-mobile__button .button').forEach((buttonEl) => {
+    buttonEl.onclick = function () {
+        const type = this.getAttribute('data-type')
+        document.querySelector(`.pricing-mobile__header-item_${type}`).click()
+    }
+})

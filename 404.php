44f13b51fc1245fe12c7a0e287ee3544ebc20cf7
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-11-08 18:36:44 +0300

    added GTM

--- 404.php
+++ 404.php
@@ -16,6 +16,24 @@ if (!defined('ABSPATH')) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
     <?php wp_head(); ?>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5L6RDDK6');
+    </script>
+    <!-- End Google Tag Manager -->
     <script type="text/javascript">
         <?php
         $templateName = (wp_is_mobile() ? 'mobile' : 'desktop') . '_p404.js';
@@ -26,6 +44,9 @@ if (!defined('ABSPATH')) {
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5L6RDDK6" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <?php
     $template = wp_is_mobile() ? 'backend/components/mobile/404' : 'backend/components/desktop/404';
     get_template_part($template);
